5926bb716e6a86fef7d3fbecad9b1fdfa8aaccc7
ohotuowo-morgan <125ohotumorgan@gmail.com>
2025-09-13 02:14:14 +0100

    week1 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Dynamically insert current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Insert last modified date
+document.getElementById("lastModified").textContent = 
+  "Last Modified: " + document.lastModified;
